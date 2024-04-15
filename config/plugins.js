fb70fc0087ea764cda848e950768495254274fd6
Ali Najmabadi <alinajmabadizadeh2002@gmail.com>
2024-04-15 12:10:11 +0330

    update env names

--- config/plugins.js
+++ config/plugins.js
@@ -3,14 +3,14 @@ module.exports = ({ env }) => ({
       config: {
         provider: 'aws-s3',
         providerOptions: {
-          accessKeyId: env('AWS_ACCESS_KEY_ID'),
-          secretAccessKey: env('AWS_ACCESS_SECRET'),
-          region: env('AWS_REGION'),
-          endpoint: env('AWS_ENDPOINT'), // Add endpoint variable here
+          accessKeyId: env('LIARA_ACCESS_KEY_ID'),
+          secretAccessKey: env('LIARA_ACCESS_SECRET'),
+          region: env('LIARA_REGION'),
+          endpoint: env('LIARA_ENDPOINT'), // Add endpoint variable here
           params: {
             ACL: env('AWS_ACL', 'public-read'),
             signedUrlExpires: env('AWS_SIGNED_URL_EXPIRES', 15 * 60),
-            Bucket: env('AWS_BUCKET'),
+            Bucket: env('LIARA_BUCKET'),
           },
         },
         actionOptions: {
