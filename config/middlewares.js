722544f1639cce8f9dc4d0ab84c1b558fb10f350
Ali Najmabadi <alinajmabadizadeh2002@gmail.com>
2024-04-15 11:47:37 +0330

    add s3 conf

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,12 +1,42 @@
-module.exports = [
-  'strapi::logger',
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
-];
+    // ~/strapi-aws-s3/backend/config/middlewares.js
+    
+    module.exports = [
+      'strapi::errors',
+      /* Replace 'strapi::security', with this snippet */
+      /* Beginning of snippet */
+      {
+        name: 'strapi::security',
+        config: {
+          contentSecurityPolicy: {
+            useDefaults: true,
+            directives: {
+              'connect-src': ["'self'", 'https:'],
+              'img-src': [
+                "'self'",
+                'data:',
+                'blob:',
+                'dl.airtable.com',
+                'some.storage.iran.liara.space',
+              ],
+              'media-src': [
+                "'self'",
+                'data:',
+                'blob:',
+                'dl.airtable.com',
+                'some.storage.iran.liara.space',
+              ],
+              upgradeInsecureRequests: null,
+            },
+          },
+        },
+      },
+      /* End of snippet */
+      'strapi::cors',
+      'strapi::poweredBy',
+      'strapi::logger',
+      'strapi::query',
+      'strapi::body',
+      'strapi::session',
+      'strapi::favicon',
+      'strapi::public',
+    ];
